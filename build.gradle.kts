55034503f6effdf393d7ef407375f39cb7e12f15
Chloe Dawn <chloe@sapphic.dev>
2021-07-26 15:45:10 +0100

    Update project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,121 @@
+import java.time.Instant
+
 plugins {
-  id("fabric-loom") version "0.4.28"
+  id("fabric-loom") version "0.8.21"
+  id("net.nemerosa.versioning") version "be24b23"
   id("signing")
 }
 
 group = "dev.sapphic"
-version = "1.1.0"
+version = "1.0.0"
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = sourceCompatibility
+  withSourcesJar()
 }
 
-minecraft {
+loom {
   refmapName = "mixins/frameless/refmap.json"
-  runDir = "run"
+  runs {
+    configureEach {
+      vmArg("-Dmixin.debug=true")
+      vmArg("-Dmixin.debug.export.decompile=false")
+      vmArg("-Dmixin.debug.verbose=true")
+      vmArg("-Dmixin.dumpTargetOnFailure=true")
+      vmArg("-Dmixin.checks=true")
+      vmArg("-Dmixin.hotSwap=true")
+    }
+  }
+}
+
+repositories {
+  maven("https://maven.terraformersmc.com/releases") {
+    content {
+      includeGroup("com.terraformersmc")
+    }
+  }
 }
 
 dependencies {
-  minecraft("com.mojang:minecraft:1.16.1")
-  mappings("net.fabricmc:yarn:1.16.1+build.5:v2")
-  modImplementation("net.fabricmc:fabric-loader:0.8.8+build.202")
-  implementation("org.checkerframework:checker-qual:3.4.1")
+  minecraft("com.mojang:minecraft:1.17.1")
+  mappings(loom.officialMojangMappings())
+  modImplementation("net.fabricmc:fabric-loader:0.11.6")
+  implementation("org.jetbrains:annotations:21.0.1")
+  implementation("org.checkerframework:checker-qual:3.15.0")
+  modRuntime("com.terraformersmc:modmenu:2.0.3")
 }
 
-tasks.withType<ProcessResources> {
-  filesMatching("/fabric.mod.json") {
-    expand("version" to version)
+tasks {
+  compileJava {
+    with(options) {
+      release.set(16)
+      isFork = true
+      isDeprecation = true
+      encoding = "UTF-8"
+      compilerArgs.addAll(listOf("-Xlint:all", "-parameters"))
+    }
+  }
+
+  processResources {
+    filesMatching("/fabric.mod.json") {
+      expand("version" to project.version)
+    }
+  }
+
+  jar {
+    from("/LICENSE")
+
+    manifest.attributes(
+      "Build-Timestamp" to Instant.now(),
+      "Build-Revision" to versioning.info.commit,
+      "Build-Jvm" to "${
+        System.getProperty("java.version")
+      } (${
+        System.getProperty("java.vendor")
+      } ${
+        System.getProperty("java.vm.version")
+      })",
+      "Built-By" to GradleVersion.current(),
+
+      "Implementation-Title" to project.name,
+      "Implementation-Version" to project.version,
+      "Implementation-Vendor" to project.group,
+
+      "Specification-Title" to "FabricMod",
+      "Specification-Version" to "1.0.0",
+      "Specification-Vendor" to project.group,
+
+      "Sealed" to "true"
+    )
+  }
+
+  assemble {
+    dependsOn(versionFile)
   }
 }
 
-tasks.withType<JavaCompile> {
-  with(options) {
-    isFork = true
-    isDeprecation = true
-    encoding = "UTF-8"
-    compilerArgs.addAll(listOf(
-      "-Xlint:all", "-parameters"
-    ))
-  }
-}
+if (hasProperty("signing.mods.keyalias")) {
+  val alias = property("signing.mods.keyalias")
+  val keystore = property("signing.mods.keystore")
+  val password = property("signing.mods.password")
 
-signing {
-  useGpgCmd()
-  sign(configurations.archives.get())
+  listOf(tasks.remapJar, tasks.remapSourcesJar).forEach {
+    it.get().doLast {
+      if (!project.file(keystore!!).exists()) {
+        error("Missing keystore $keystore")
+      }
+
+      val file = outputs.files.singleFile
+      ant.invokeMethod(
+        "signjar", mapOf(
+          "jar" to file,
+          "alias" to alias,
+          "storepass" to password,
+          "keystore" to keystore,
+          "verbose" to true,
+          "preservelastmodified" to true
+        )
+      )
+      signing.sign(file)
+    }
+  }
 }
--- src/main/java/dev/sapphic/frameless/ItemFrameMixin.java
+++ src/main/java/dev/sapphic/frameless/ItemFrameMixin.java
@@ -1,15 +1,16 @@
 package dev.sapphic.frameless;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.decoration.AbstractDecorationEntity;
-import net.minecraft.entity.decoration.ItemFrameEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.sound.SoundEvents;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.decoration.HangingEntity;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
@@ -17,31 +18,33 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ItemFrameEntity.class)
-abstract class ItemFrameMixin extends AbstractDecorationEntity {
-  ItemFrameMixin() {
-    //noinspection ConstantConditions
-    super(null, null);
+@Mixin(ItemFrame.class)
+abstract class ItemFrameMixin extends HangingEntity {
+  ItemFrameMixin(final EntityType<? extends HangingEntity> type, final Level level) {
+    super(type, level);
   }
 
   @Inject(
-    method = "damage",
+    method = "hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z",
     at = @At(
-      value = "INVOKE",
-      target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;getHeldItemStack()Lnet/minecraft/item/ItemStack;",
-      shift = Shift.BEFORE),
-    cancellable = true, require = 1, allow = 1)
+      shift = Shift.BEFORE, value = "INVOKE", opcode = Opcodes.INVOKEVIRTUAL,
+      target = "Lnet/minecraft/world/entity/decoration/ItemFrame;getItem()Lnet/minecraft/world/item/ItemStack;"),
+    require = 1, allow = 1, cancellable = true)
   private void tryHide(final DamageSource source, final float amount, final CallbackInfoReturnable<Boolean> cir) {
-    if (!this.isInvisible() && !this.world.isClient) {
-      final @Nullable Entity attacker = source.getAttacker();
-      if (attacker instanceof PlayerEntity) {
-        final ItemStack held = ((LivingEntity) attacker).getMainHandStack();
+    if (!this.isInvisible() && !this.level.isClientSide) {
+      final @Nullable Entity attacker = source.getEntity();
+
+      if (attacker instanceof final Player player) {
+        final var held = player.getMainHandItem();
+
         if (held.getItem() == Items.GLASS_PANE) {
-          if (!((PlayerEntity) attacker).abilities.creativeMode) {
-            held.decrement(1);
+          if (!player.getAbilities().instabuild) {
+            held.shrink(1);
           }
-          this.playSound(SoundEvents.ENTITY_ITEM_FRAME_PLACE, 1.0F, 1.0F);
+
+          this.playSound(SoundEvents.ITEM_FRAME_PLACE, 1.0F, 1.0F);
           this.setInvisible(true);
+
           cir.setReturnValue(true);
         }
       }
@@ -49,15 +52,14 @@ abstract class ItemFrameMixin extends AbstractDecorationEntity {
   }
 
   @Inject(
-    method = "dropHeldStack",
+    method = "dropItem(Lnet/minecraft/world/entity/Entity;Z)V",
     at = @At(
-      value = "INVOKE",
-      target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;dropItem(Lnet/minecraft/item/ItemConvertible;)Lnet/minecraft/entity/ItemEntity;",
-      shift = Shift.AFTER),
+      shift = Shift.AFTER, value = "INVOKE", opcode = Opcodes.INVOKEVIRTUAL,
+      target = "Lnet/minecraft/world/entity/decoration/ItemFrame;spawnAtLocation(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/entity/item/ItemEntity;"),
     require = 1, allow = 1)
-  private void dropPane(final CallbackInfo ci) {
+  private void tryDropPane(final CallbackInfo ci) {
     if (this.isInvisible()) {
-      this.dropItem(Items.GLASS_PANE);
+      this.spawnAtLocation(Items.GLASS_PANE);
     }
   }
 }
