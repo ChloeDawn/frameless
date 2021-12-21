9e33d028da027755c6389ca96f9ca71688244dbf
Chloe Dawn <chloe@sapphic.dev>
2021-12-21 17:57:24 +0000

    Reformat code, add license headers

--- src/main/java/dev/sapphic/frameless/ItemFrameMixin.java
+++ src/main/java/dev/sapphic/frameless/ItemFrameMixin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Chloe Dawn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.sapphic.frameless;
 
 import net.minecraft.sounds.SoundEvents;
@@ -24,12 +40,23 @@ abstract class ItemFrameMixin extends HangingEntity {
   }
 
   @Inject(
-    method = "hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z",
-    at = @At(
-      shift = Shift.BEFORE, value = "INVOKE", opcode = Opcodes.INVOKEVIRTUAL,
-      target = "Lnet/minecraft/world/entity/decoration/ItemFrame;getItem()Lnet/minecraft/world/item/ItemStack;"),
-    require = 1, allow = 1, cancellable = true)
-  private void trySetInvisible(final DamageSource source, final float amount, final CallbackInfoReturnable<Boolean> callback) {
+      method = "hurt(" + "Lnet/minecraft/world/damagesource/DamageSource;" + "F" + ")Z",
+      at =
+          @At(
+              shift = Shift.BEFORE,
+              value = "INVOKE",
+              opcode = Opcodes.INVOKEVIRTUAL,
+              target =
+                  "Lnet/minecraft/world/entity/decoration/ItemFrame;"
+                      + "getItem()"
+                      + "Lnet/minecraft/world/item/ItemStack;"),
+      require = 1,
+      allow = 1,
+      cancellable = true)
+  private void trySetInvisible(
+      final DamageSource source,
+      final float amount,
+      final CallbackInfoReturnable<Boolean> callback) {
     if (!this.level.isClientSide && !this.isInvisible() && source.getEntity() instanceof Player) {
       final Player player = (Player) source.getEntity();
       final ItemStack stack = player.getMainHandItem();
@@ -48,12 +75,19 @@ abstract class ItemFrameMixin extends HangingEntity {
   }
 
   @Inject(
-    method = "dropItem(Lnet/minecraft/world/entity/Entity;Z)V",
-    at = @At(
-      shift = Shift.AFTER, value = "INVOKE", opcode = Opcodes.INVOKEVIRTUAL, ordinal = 0,
-      target = "Lnet/minecraft/world/entity/decoration/ItemFrame;spawnAtLocation"
-        + "(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/entity/item/ItemEntity;"),
-    require = 1)
+      method = "dropItem(" + "Lnet/minecraft/world/entity/Entity;" + "Z" + ")V",
+      at =
+          @At(
+              shift = Shift.AFTER,
+              value = "INVOKE",
+              opcode = Opcodes.INVOKEVIRTUAL,
+              ordinal = 0,
+              target =
+                  "Lnet/minecraft/world/entity/decoration/ItemFrame;"
+                      + "spawnAtLocation("
+                      + "Lnet/minecraft/world/item/ItemStack;"
+                      + ")Lnet/minecraft/world/entity/item/ItemEntity;"),
+      require = 1)
   private void tryDropPane(final CallbackInfo callback) {
     if (this.isInvisible()) {
       this.spawnAtLocation(Items.GLASS_PANE);
